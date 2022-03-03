9357eccab60b0e91bbb1efa573ff035577c3bed4
lucho23fuentes88 <69926848+lucho23fuentes88@users.noreply.github.com>
2022-03-03 09:03:34 -0300

    acerdaDe

--- ProyectoIntegrador_LGF/src/app/app.module.ts
+++ ProyectoIntegrador_LGF/src/app/app.module.ts
@@ -8,13 +8,17 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
